a3bc010cfcc7faa95950699fdf7dafcc503a150d
Ravindra898959 <ravindra.064464@gmail.com>
2022-12-08 13:37:29 +0530

    changes done

--- src/Home.js
+++ src/Home.js
@@ -5,18 +5,6 @@ const Home = () => {
   // const [modal, setModal] = useState(false);
   return (
     <>
-      {/* <div className="home-modal">
-        <h1>Are you sure you want to open the modal </h1>
-        <button
-          className="btn"
-          onClick={() => {
-            setModal(true);
-          }}
-        >
-          Open
-        </button>
-        {modal && <Modal closeModal={setModal} />}
-      </div> */}
       <HeaderContent />
     </>
   );
